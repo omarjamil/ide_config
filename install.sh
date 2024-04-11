81525513e8900532a9626df075d6d0203929d9f7
Omar Jamil <omar.jamil@alephinsights.com>
2024-04-11 15:57:12 +0100

    home area as argument

--- install.sh
+++ install.sh
@@ -1,28 +1,29 @@
-cd 
+# The argument needs to be the path to the home area
+cd $1
 mkdir config_backup
 mv .gitconfig .git_ignore .emacs .emacs.d .bashrc .bash_profile .vimrc .vim .bash config_backup
 mkdir .config
 
 # create softlinks 
-ln -s ~/ide_config/bash ~/.bash
-ln -s ~/ide_config/bashrc ~/.bashrc
-ln -s ~/ide_config/zsh ~/.zsh
-ln -s ~/ide_config/zshrc ~/.zshrc
-ln -s ~/ide_config/bash_profile ~/.bash_profile
-ln -s ~/ide_config/shell ~/.shell
-ln -s ~/ide_config/tmux.conf ~/.tmux.conf
-ln -s ~/ide_config/nvim ~/.config/nvim
-ln -s ~/ide_config/zsh/p10k.zsh ~/.p10k.zsh
+ln -s $1/ide_config/bash $1/.bash
+ln -s $1/ide_config/bashrc $1/.bashrc
+ln -s $1/ide_config/zsh $1/.zsh
+ln -s $1/ide_config/zshrc $1/.zshrc
+ln -s $1/ide_config/bash_profile $1/.bash_profile
+ln -s $1/ide_config/shell $1/.shell
+ln -s $1/ide_config/tmux.conf $1/.tmux.conf
+ln -s $1/ide_config/nvim $1/.config/nvim
+ln -s $1/ide_config/zsh/p10k.zsh $1/.p10k.zsh
 
 # echo "installing zinit"
 # bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
 
 # echo "installing zsh-autosuggestions"
-# git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-autosuggestions $1/.zsh/zsh-autosuggestions
 
 echo "Installing fzf"
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.local/fzf
-~/.local/fzf/install --all
+git clone --depth 1 https://github.com/junegunn/fzf.git $1/.local/fzf
+$1/.local/fzf/install --all
 
 echo "Installing Homebrew"
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
