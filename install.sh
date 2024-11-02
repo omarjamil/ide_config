1b632dbe6a13dd046b0edc7349e7c42da687b444
Omar Jamil <oj249@cam.ac.uk>
2024-11-02 16:38:12 +0000

    Changed argument to HOME

--- install.sh
+++ install.sh
@@ -1,29 +1,28 @@
-# The argument needs to be the path to the home area
-cd $1
+cd $HOME
 mkdir config_backup
 mv .gitconfig .git_ignore .emacs .emacs.d .bashrc .bash_profile .vimrc .vim .bash config_backup
 mkdir .config
 
 # create softlinks 
-ln -s $1/ide_config/bash $1/.bash
-ln -s $1/ide_config/bashrc $1/.bashrc
-ln -s $1/ide_config/zsh $1/.zsh
-ln -s $1/ide_config/zshrc $1/.zshrc
-ln -s $1/ide_config/bash_profile $1/.bash_profile
-ln -s $1/ide_config/shell $1/.shell
-ln -s $1/ide_config/tmux.conf $1/.tmux.conf
-ln -s $1/ide_config/nvim $1/.config/nvim
-ln -s $1/ide_config/zsh/p10k.zsh $1/.p10k.zsh
+ln -s $HOME/ide_config/bash $HOME/.bash
+ln -s $HOME/ide_config/bashrc $HOME/.bashrc
+ln -s $HOME/ide_config/zsh $HOME/.zsh
+ln -s $HOME/ide_config/zshrc $HOME/.zshrc
+ln -s $HOME/ide_config/bash_profile $HOME/.bash_profile
+ln -s $HOME/ide_config/shell $HOME/.shell
+ln -s $HOME/ide_config/tmux.conf $HOME/.tmux.conf
+ln -s $HOME/ide_config/nvim $HOME/.config/nvim
+ln -s $HOME/ide_config/zsh/p10k.zsh $HOME/.p10k.zsh
 
 # echo "installing zinit"
 # bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
 
 # echo "installing zsh-autosuggestions"
-# git clone https://github.com/zsh-users/zsh-autosuggestions $1/.zsh/zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.zsh/zsh-autosuggestions
 
 echo "Installing fzf"
-git clone --depth 1 https://github.com/junegunn/fzf.git $1/.local/fzf
-$1/.local/fzf/install --all
+git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.local/fzf
+$HOME/.local/fzf/install --all
 
 # echo "Installing Homebrew"
 # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
